56c1d9f08a98a5efc86404521573f250505eb22d
shira-miller <sm0504123007@gmail.com>
2025-10-19 12:45:43 +0300

    import

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import config from "../secret/config.js";
 export async function connectDB() {
   try {
     await mongoose.connect(`mongodb+srv://${config.userDB}:${config.passDB}@cluster0.a9w5hjt.mongodb.net/top_cat_gaming`);
