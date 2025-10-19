484d7eaf63020626706e08e74bbad8e8566ae3df
shira-miller <sm0504123007@gmail.com>
2025-10-19 13:38:23 +0300

    move

--- server/server.js
+++ server/server.js
@@ -6,27 +6,20 @@ import { routesInit } from "./routes/config_routes.js";
 import "./db/connect.js";
 import { fileURLToPath } from "url";
 
-// __dirname ב‑ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// יצירת אפליקציה
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Static files
 app.use(express.static(path.join(__dirname, "public")));
 
-// Routes
 routesInit(app);
 
-// יצירת שרת HTTP
 const server = http.createServer(app);
 
-// Port
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
